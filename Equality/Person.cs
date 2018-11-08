05b629a4d0be267d6711216fa636a50a1c0d0db8
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2018-11-08 02:38:19 +0200

    added equality sample

---
+++ Equality/Person.cs
@@ -0,0 +1,54 @@
+﻿namespace Equality
+{
+    public class Person
+    {
+        private string personName;
+
+        public Person(string name)
+        {
+            this.personName = name;
+        }
+
+        public override string ToString()
+        {
+            return this.personName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            //obj.personName is accesible although it is private because we are
+            //accessing it from within the same class
+            return this.personName.Equals(((Person)obj).personName);
+        }
+
+        // == operator overloading
+        public static bool operator ==(Person person1, Person person2)
+        {
+            if (person1 == null && person2 == null)
+            {
+                return true;
+            }
+
+            if(person1 != null)
+            {
+                return person1.Equals(person2);
+            }
+            return false;
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !(person1 == person2);
+        }
+    }
+}
---
+++ Equality/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1a = new Person("John");
+            Person person1b = person1a;
+            Person person2 = new Person(person1a.ToString());
+
+            Console.WriteLine("person1a and person1b: {0}", person1a.Equals(person1b));
+            Console.WriteLine($"person1a == person1b? {person1a == person1b}");
+            Console.WriteLine("person1a and person2: {0}", person1a.Equals(person2));
+
+            //Cast to an Object and call Equals
+            Console.WriteLine("person1a and person1b: {0}", ((object)person1a).Equals((object)person1b));
+            Console.WriteLine("person1a and person2: {0}", ((object)person1a).Equals((object)person2));
+
+            Person empty = null;
+            Person notEmpty = new Person("test");
+            Console.WriteLine(empty == notEmpty);
+
+            // If the current instance is a value type, the Equals(Object) method tests for value equality.
+            // Value equality means that The two objects are of the same type.
+            // the following example shows, a Byte object that has a value of 12 does not equal an Int32 object that has a value of 12, 
+            // because the two objects have different run-time types.
+            byte value1 = 12;
+            int value2 = 12;
+
+            Console.WriteLine(value1 == value2);
+
+            object object1 = value1; //boxing
+            object object2 = value2; //boxing
+
+            Console.WriteLine("{0} ({1}) = {2} ({3}): {4}",
+                              object1, object1.GetType().Name,
+                              object2, object2.GetType().Name,
+                              object1.Equals(object2));
+            //beware of comparisons / equality checks between different types
+        }
+    }
+}

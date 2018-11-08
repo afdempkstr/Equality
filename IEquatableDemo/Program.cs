40aa650823868d927b8a28e4762d85246e9dc5a2
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2018-11-08 02:45:03 +0200

    added IEquatabe demo

---
+++ IEquatableDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEquatableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Vehicle();
+            Vehicle v2 = new Vehicle("My bicycle", 60);
+            Vehicle v3 = new Vehicle("Bugatti Chiron", 467);
+            Vehicle v4 = new Vehicle("Airbus A380", 1020);
+            Vehicle v5 = null;
+
+            //initialize a list of vehicles and add all these vehicles
+            var vehicles = new List<Vehicle>() { v1, v2, v3, v4, v5 };
+
+            //compare each vehicle in the list to the rest using the default equality
+            foreach (var v in vehicles)
+            {
+                Console.WriteLine($"{v} equals {v1} = {object.Equals(v, v1)}");
+                Console.WriteLine($"{v} equals {v2} = {object.Equals(v, v2)}");
+                Console.WriteLine($"{v} equals {v3} = {object.Equals(v, v3)}");
+                Console.WriteLine($"{v} equals {v4} = {object.Equals(v, v4)}");
+                Console.WriteLine($"{v} equals {v5} = {object.Equals(v, v5)}");
+            }
+
+        }
+    }
+}
---
+++ IEquatableDemo/Vehicle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace IEquatableDemo
+{
+    class Vehicle : IEquatable<Vehicle>
+    {
+        public int Speed { get; protected set; }
+
+        public string Name { get; protected set; }
+
+        public Vehicle() { }
+
+        public Vehicle(string name, int speed)
+        {
+            Speed = speed;
+            Name = name;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other == null)
+                return false;
+
+            if (object.ReferenceEquals(this, other))
+                return true;
+
+            if (this.GetType() != other.GetType())
+                return false;
+            // this will work for the Vehicle class and its subclasses
+            //if (!(other is Vehicle))
+            //    return false;
+
+            // the as keyword is like a cast that returns null if unsuccessful
+            return this.Equals(other as Vehicle);
+        }
+
+        public bool Equals(Vehicle other)
+        {
+            if (other == null)
+                return false;
+
+            if (object.ReferenceEquals(this, other))
+                return true;
+
+            if (this.GetType() != other.GetType())
+                return false;
+
+            if (this.Name == other.Name && this.Speed.Equals(other.Speed))
+                return true;
+            else
+                return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}: {Name}@{Speed}km/h";
+        }
+    }
+}
